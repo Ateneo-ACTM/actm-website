885a315afba261680caaf60a8bf2ea96157bfe70
Josh Taningco <josh.taningco@gmail.com>
2020-09-01 14:00:48 +0800

    update nav (simple b&w)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,20 +5,14 @@ import { Nav } from "./styles"
 const Navbar = ({ slug }) => (
   <Nav>
     <div className='navbar-container'>
-      <Link to="#" className='navbar-logo'>
-        ACTM
-      </Link>
+      <Link to="#" className='navbar-logo'>ACTM</Link>
       <div className='menu-icon'></div>
       <ul className='nav-menu'>
         <li className='nav-item'>
-          <Link to='#' className='nav-links'>
-            Home
-          </Link>
+          <Link to='#' className='nav-links'>Home</Link>
         </li>
         <li className='nav-item'>
-          <Link to='#' className='nav-links'>
-            About
-          </Link>
+          <Link to='#' className='nav-links'>About</Link>
         </li>
       </ul>
     </div>
