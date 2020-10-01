9e45ccf0703f6274965ad001f623a372d87c47fe
Josh Taningco <josh.taningco@gmail.com>
2020-10-02 04:17:01 +0800

    about page mobile responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,16 +18,15 @@ import {
     // Acads
     Acads,
     AcadsContainer,
+    AcadsWrapper,
     AcadsContent,
     AcadsHeader,
     AcadsSub,
     AcadsText,
     AcadsButton,
-    AcadsImg
+    AcadsImg,
+    AcadsImgMobile
 } from "./styles"
-
-import what from "../../templates/about-page/assets/images/about_2.svg"
-import what2 from "../../templates/about-page/assets/images/about_3.svg"
 
 function About() {
     return(
@@ -43,20 +42,20 @@ function About() {
             <WhatTitle>What We Stand For</WhatTitle>
             <WhatContainer>
                 <WhatWrapper>
+                    <WhatContent className="a" />
                     <WhatContent className="left">
-                        <WhatHeader>We believe that there is a
+                        <WhatHeader className="left-m">We believe that there is a
                             <HeaderSpan>{`\n`}solid space for creativity{`\n`}</HeaderSpan>
                             in the world of business.</WhatHeader>
-                        <WhatText>ACTM strives to uphold an environment that motivates creative thinkers, and is safe for whatever creative ideas they might produce. New ideas are what drive us all forward, and allow us to constantly improve the way we approach our work.</WhatText>
+                        <WhatText className="left-m">ACTM strives to uphold an environment that motivates creative thinkers, and is safe for whatever creative ideas they might produce. New ideas are what drive us all forward, and allow us to constantly improve the way we approach our work.</WhatText>
                     </WhatContent>
-                    <WhatContent style={{backgroundImage: `url(${what})`}} />
                 </WhatWrapper>
                 <WhatWrapper>
-                    <WhatContent style={{backgroundImage: `url(${what2})`}} />
+                    <WhatContent className="b" />
                         <WhatContent className="right">
-                            <WhatHeader>We believe in the
+                            <WhatHeader className="right-m">We believe in the
                                 <HeaderSpan>{`\n`}importance of empathy.</HeaderSpan></WhatHeader>
-                            <WhatText>ACTM goes the extra mile to understand the feelings and context of our members, as well as the target market of the projects we promote. We strive to promote an environment that trains empathetic, socially responsible practitioners of Integrated Marketing Communications, who have the ability to frame ideas into stories that inform and inspire.</WhatText>
+                            <WhatText className="right-m">ACTM goes the extra mile to understand the feelings and context of our members, as well as the target market of the projects we promote. We strive to promote an environment that trains empathetic, socially responsible practitioners of Integrated Marketing Communications, who have the ability to frame ideas into stories that inform and inspire.</WhatText>
                         </WhatContent>
                 </WhatWrapper>
             </WhatContainer>
@@ -64,13 +63,16 @@ function About() {
 
             <Acads>
                 <AcadsContainer>
-                    <AcadsContent>
-                        <AcadsHeader>Academics</AcadsHeader>
-                        <AcadsSub>BS COMTECH Program</AcadsSub>
-                        <AcadsText>Bachelor of Science in Communications Technology Management is a 4-year undergraduate program in the John Gokongwei School of Management. The curriculum is patterned after the BS Management Program, with an integration of subjects in communications and information technology. Through the program, students are exposed to both managerial and creative processes that equip them with the necessary skills and knowledge to work in multiple industries. Graduates of the course are known to enter the different fields of marketing, communications, media, advertising, and information technology.</AcadsText>
-                        <AcadsButton to="">Learn More</AcadsButton>
-                    </AcadsContent>
-                    <AcadsContent><AcadsImg /></AcadsContent>
+                <AcadsHeader>Academics</AcadsHeader>
+                    <AcadsWrapper>
+                        <AcadsContent>
+                            <AcadsSub>BS COMTECH Program</AcadsSub>
+                            <AcadsImgMobile />
+                            <AcadsText>Bachelor of Science in Communications Technology Management is a 4-year undergraduate program in the John Gokongwei School of Management. The curriculum is patterned after the BS Management Program, with an integration of subjects in communications and information technology. Through the program, students are exposed to both managerial and creative processes that equip them with the necessary skills and knowledge to work in multiple industries. Graduates of the course are known to enter the different fields of marketing, communications, media, advertising, and information technology.</AcadsText>
+                            <AcadsButton to="">Learn More</AcadsButton>
+                        </AcadsContent>
+                        <AcadsContent><AcadsImg /></AcadsContent>
+                    </AcadsWrapper>
                 </AcadsContainer>
             </Acads>
         </AboutContainer>
--- src/components/About/styles.jsx
+++ src/components/About/styles.jsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import { Link } from "gatsby"
 import who from "../../templates/about-page/assets/images/about_1.svg"
+import who_m from "../../templates/about-page/assets/images/about_1_m.svg"
+import what from "../../templates/about-page/assets/images/about_2.svg"
+import what_1_m from "../../templates/about-page/assets/images/about_2_m.svg"
+import what_2 from "../../templates/about-page/assets/images/about_3.svg"
+import what_2_m from "../../templates/about-page/assets/images/about_3_m.svg"
 import '../Layout/styles.css';
 
 export const AboutContainer = styled.div`
@@ -15,6 +20,14 @@ export const Who = styled.section`
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
+
+    @media screen and (max-width: 480px) {
+        min-height: 160vh; 
+        background-image: url(${who_m});
+        background-size: cover;
+        background-position: center;
+        border-radius: 0px 40px 0px 0px;
+    }   
 `
 
 export const WhoContent = styled.div`
@@ -27,6 +40,15 @@ export const WhoContent = styled.div`
     justify-content: flex-start;
     align-content: center;
     align-items: center;
+
+    @media screen and (max-width: 480px) {
+        right: 40%;
+        top: 40%;
+        width: 100%;
+        height: 90%;
+        justify-content: flex-end;
+        align-items: flex-start;
+    }
 `
 
 export const WhoHeader = styled.h1`
@@ -35,12 +57,25 @@ export const WhoHeader = styled.h1`
     font-weight: 400;
     margin: 0 0 25px 0;
     width: 550px;
+
+    @media screen and (max-width: 480px) {
+        margin: 0;
+        font-size: 2rem;
+        width: 360px;
+    }
 `
 
 export const WhoText = styled.p`
     text-align: left;
     font-size: 0.8rem;
+    line-height: 175%;
     width: 550px;
+
+    @media screen and (max-width: 480px) {
+        margin: 15px 0 0 0;
+        font-size: 0.75rem;
+        width: 280px;
+    }
 `
 
 export const What = styled.section`
@@ -58,7 +93,7 @@ export const WhatTitle = styled.h2`
     align-content: center;
     align-items: center;
     text-align: center;
-    padding: 100px 0 50px 0;
+    padding-top: 100px;
     width: 100%;
     color: white;
 `
@@ -69,6 +104,9 @@ export const WhatContainer = styled.div`
     justify-content: center;
     align-items: center;
     align-content: center;
+    width: 100%;
+    height: 100%;
+    padding: 10vh 0;
 `
 
 export const WhatWrapper = styled.div`
@@ -78,6 +116,15 @@ export const WhatWrapper = styled.div`
     align-items: center;
     align-content: center;
     margin: 50px 0;
+
+    &:nth-child(1) {
+        flex-direction: row-reverse;
+    }
+
+    @media screen and (max-width: 480px) { 
+        display: block;
+        margin: 25px 0;
+    }
 `
 
 export const WhatContent = styled.div`
@@ -94,6 +141,19 @@ export const WhatContent = styled.div`
 
     &.left { padding-left: 30px; }
     &.right { padding-right: 30px; }
+    &.a { background-image: url(${what}) }
+    &.b { background-image: url(${what_2}) }
+
+    @media screen and (max-width: 480px) { 
+        width: 90vw;
+        min-height: 40vw;
+        background-size: contain;
+        
+        &.left { align-items: flex-start; padding-left: 20px; }
+        &.right { align-items: flex-end;  padding-right: 20px; }
+        &.a { background-image: url(${what_1_m}) }
+        &.b { background-image: url(${what_2_m}) }
+    }
 `
 
 export const WhatHeader = styled.h1`
@@ -103,12 +163,23 @@ export const WhatHeader = styled.h1`
     margin-bottom: 40px;
     text-align: center;
     color: white;
+    line-height: 110%;
+
+    @media screen and (max-width: 480px) {
+        width: 75vw;
+        font-weight: 600;
+
+        &.left-m { text-align: left; }
+        &.right-m { text-align: right; }
+    } 
 `
 
 export const HeaderSpan = styled.span`
     font-weight: 600;
     color: #FEC200;
     white-space: pre-wrap;
+
+    @media screen and (max-width: 480px) { font-weight: 900; }
 `
 
 export const WhatText = styled.p`
@@ -120,41 +191,80 @@ export const WhatText = styled.p`
     text-align: left;
     width: 380px;
     color: white;
+
+    @media screen and (max-width: 480px) { 
+        font-size: 0.75rem; 
+        width: 80vw; 
+        
+        &.left-m { text-align: left; }
+        &.right-m { text-align: right; }
+    }
 `
 
 export const Acads = styled.section`
     width: 100vw;
     min-height: 100vh;
-`
-
-export const AcadsContainer = styled.div`
     display: flex;
     justify-content: center;
     align-content: center;
     align-items: center;
-    min-height: 80vh;
-`
-
-export const AcadsContent = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: flex-start;
-    align-content: center;
-    width: 40vw;
-    min-height: inherit;
 `
 
 export const AcadsHeader = styled.h1`
     font-size: 2rem;
     font-weight: 400;
+    padding: 100px 0 40px 0;
+    width: 80%;
+    color: black;
+`
+
+export const AcadsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+    width: 100%;
+`
+
+export const AcadsWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    width: 100%;
+    height: 100%;
+
+    @media screen and (max-width: 480px) { flex-direction: column; }
+`
+
+export const AcadsContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    align-content: center;
+    height: 320px;
+    width: 40vw;
+
+    @media screen and (max-width: 480px) { 
+        height: 100%;
+        width: 80vw; 
+        justify-content: center;
+        align-items: center;
+    }
 `
 
 export const AcadsSub = styled.h2`
     font-size: 1rem;
     font-weight: 400;
     display: flex;
-    padding: 50px 0 40px 0;
+
+    @media screen and (max-width: 480px) {
+        justify-content: flex-start;
+        width: 100%;
+    }
 `
 
 export const AcadsText = styled.p`
@@ -163,15 +273,18 @@ export const AcadsText = styled.p`
     line-height: 175%;
     text-align: justify;
     width: 500px;
+
+    @media screen and (max-width: 480px) { width: 100%; }
 `
 
 export const AcadsButton = styled(Link)`
     appearance: button;
-    margin-top: 50px;
     padding: 5px 20px;
     background: linear-gradient(90deg, #3FE0E0 0%, #21B1CF 100%);
     border-radius: 8px;
     color: white;
+
+    @media screen and (max-width: 480px) { margin: 10vh 0 15vh 0; }
 `
 
 export const AcadsImg = styled.img`
@@ -180,8 +293,25 @@ export const AcadsImg = styled.img`
     align-items: center;
     align-content: center;
     width: 100%;
-    height: 400px;
+    height: 100%;
     background: #E5E5E5;
     border: 1px solid rgba(0,0,0,0.3);
     border-radius: 30px;
+
+    @media screen and (max-width: 480px) { display: none; }
+`
+
+export const AcadsImgMobile = styled.img`
+    display: none;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    width: 100%;
+    height: 200px;
+    background: #E5E5E5;
+    border: 1px solid rgba(0,0,0,0.3);
+    border-radius: 15px;
+    margin: 30px 0;
+
+    @media screen and (max-width: 480px) { display: flex; }
 `
